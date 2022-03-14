12bc21754fcf2474b8b4cc155a399b2283e4d585
Devon K <dalvinderkhosa@gmail.com>
2022-03-14 15:13:45 -0700

    moved electron-shopping-list

---
+++ electron-shopping-list/main.js
@@ -0,0 +1,124 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const{app, BrowserWindow, Menu, ipcMain} = electron;
+
+
+//SET ENV
+process.env.NODE_ENV = 'production';
+
+
+let mainWindow;
+let addWindow;
+//Listen for app to be ready
+app.on('ready', function() {
+    //create new window
+    mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true
+        }
+    }
+ );
+    //Load html file into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    //Exit app when main window closed
+    mainWindow.on('closed', function(){
+        app.quit();
+    });
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+//Handle create add window
+function createAddWindow(){
+ //create new window
+ addWindow = new BrowserWindow({
+     width: 300,
+     height:200,
+     webPreferences: {
+         nodeIntegration:true
+     },
+     title:'Add Shopping List Item'
+ });
+ //Load html file into window
+ addWindow.loadURL(url.format({
+     pathname: path.join(__dirname, 'addWindow.html'),
+     protocol: 'file',
+     slashes: true
+ }));
+ // Garbage collection handle
+ addWindow.on('close', function(){
+     addWindow = null;
+ });
+}
+
+//Catch item:add
+ipcMain.on('item:add', function(e, item){
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
+
+//Create menu template
+
+const mainMenuTemplate = [
+
+    {
+        label:'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' :
+                'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+//If mac, add empty object to menu
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift();
+}
+
+//Add dev tools if not in production
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu:[
+            {
+                role: 'reload'
+            },
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' :
+                'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+        ]
+    });
+} 
---
+++ electron-shopping-list/readme.md
@@ -0,0 +1,8 @@
+# Shopping List 
+Shopping list created with HTML, JavaScript, Node and Electron. Styled with Materialize.
+## MAC OS X
+To build, first run "npm install", and then run "npm run package-mac".
+## Windows
+TBD; Currently in the works.
+###### Link to tutorial
+https://www.youtube.com/watch?v=kN1Czs0m1SU
